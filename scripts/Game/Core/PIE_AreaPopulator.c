70e8a1f39622d67f6291707edf805b341060cf6c
Piepieonline <piepieonline@gmail.com>
2025-12-13 23:18:27 +1100

    Patrols, factions, vehicles

--- scripts/Game/Core/PIE_AreaPopulator.c
+++ scripts/Game/Core/PIE_AreaPopulator.c
@@ -2,8 +2,18 @@ class PIE_AreaPopulator : Managed
 {
 	float paramSpawnRadius = 100;
 	int paramBuildingsToFill = 5;
+	int paramPatrolsCount = 5;
+	int paramVehicleCount = 2;
+	bool paramVehicleJeep = true;
+	bool paramVehicleAPC = true;
+	bool paramVehiclePatrols = false;
+	string paramSpawnUnitFaction = "USSR";
 	
 	int valueFoundBuildings = -1;
+	
+	static ref map<string, ref array<string>> unitTypes = new map<string, ref array<string>>();
+	static ref map<string, ref array<string>> jeepTypes = new map<string, ref array<string>>();
+	static ref map<string, ref array<string>> apcTypes = new map<string, ref array<string>>();
 	
 	protected static ref array<PIE_AreaPopulator> areaPopulators = new array<PIE_AreaPopulator>;
 	SCR_MapDescriptorComponent selectedMapDesc;
@@ -14,6 +24,31 @@ class PIE_AreaPopulator : Managed
 	{
 		PIE_AreaPopulator populator = new PIE_AreaPopulator();
 		populator.selectedMapDesc = mapDesc;
+		
+		populator.unitTypes.Clear();
+		array<string> usUnits = { "{84E5BBAB25EA23E5}Prefabs/Groups/BLUFOR/Group_US_FireTeam.et", "{DDF3799FA1387848}Prefabs/Groups/BLUFOR/Group_US_RifleSquad.et" };
+		array<string> ussrUnits = { "{30ED11AA4F0D41E5}Prefabs/Groups/OPFOR/Group_USSR_FireGroup.et", "{E552DABF3636C2AD}Prefabs/Groups/OPFOR/Group_USSR_RifleSquad.et" };
+		array<string> fiaUnits = { "{5BEA04939D148B1D}Prefabs/Groups/INDFOR/Group_FIA_FireTeam.et", "{CE41AF625D05D0F0}Prefabs/Groups/INDFOR/Group_FIA_RifleSquad.et" };
+		unitTypes.Set("US", usUnits);
+		unitTypes.Set("USSR", ussrUnits);
+		unitTypes.Set("FIA", fiaUnits);
+		
+		populator.jeepTypes.Clear();
+		array<string> usJeeps = { "{3EA6F47D95867114}Prefabs/Vehicles/Wheeled/M998/M1025_armed_M2HB.et", "{26A9756790131354}Prefabs/Characters/Factions/BLUFOR/US_Army/Character_US_Rifleman.et", "{26A9756790131354}Prefabs/Characters/Factions/BLUFOR/US_Army/Character_US_Rifleman.et" };
+		array<string> ussrJeeps = { "{254289B9C09904AB}Prefabs/Vehicles/Wheeled/BRDM2/BRDM2.et", "{9FFEF10757E742EB}Prefabs/Characters/Factions/OPFOR/USSR_Army/Character_USSR_Crew.et", "{9FFEF10757E742EB}Prefabs/Characters/Factions/OPFOR/USSR_Army/Character_USSR_Crew.et", "{9FFEF10757E742EB}Prefabs/Characters/Factions/OPFOR/USSR_Army/Character_USSR_Crew.et"};
+		array<string> fiaJeeps = { "{0B4DEA8078B78A9B}Prefabs/Vehicles/Wheeled/UAZ469/UAZ469_PKM.et", "{84B40583F4D1B7A3}Prefabs/Characters/Factions/INDFOR/FIA/Character_FIA_Rifleman.et", "{84B40583F4D1B7A3}Prefabs/Characters/Factions/INDFOR/FIA/Character_FIA_Rifleman.et" };
+		jeepTypes.Set("US", usJeeps);
+		jeepTypes.Set("USSR", ussrJeeps);
+		jeepTypes.Set("FIA", fiaJeeps);
+		
+		populator.apcTypes.Clear();
+		array<string> usAPCs = { "{0FBF8F010F81A4E5}Prefabs/Vehicles/Wheeled/LAV25/LAV25.et", "{E1CB513B8B9B08F4}Prefabs/Characters/Factions/BLUFOR/US_Army/Character_US_Crew.et", "{E1CB513B8B9B08F4}Prefabs/Characters/Factions/BLUFOR/US_Army/Character_US_Crew.et", "{E1CB513B8B9B08F4}Prefabs/Characters/Factions/BLUFOR/US_Army/Character_US_Crew.et" };
+		array<string> ussrAPCs = { "{C012BB3488BEA0C2}Prefabs/Vehicles/Wheeled/BTR70/BTR70.et", "{9FFEF10757E742EB}Prefabs/Characters/Factions/OPFOR/USSR_Army/Character_USSR_Crew.et", "{9FFEF10757E742EB}Prefabs/Characters/Factions/OPFOR/USSR_Army/Character_USSR_Crew.et", "{9FFEF10757E742EB}Prefabs/Characters/Factions/OPFOR/USSR_Army/Character_USSR_Crew.et" };
+		array<string> fiaAPCs = { "{442939C9617DF228}Prefabs/Vehicles/Wheeled/BRDM2/BRDM2_FIA.et", "{641AD7731E23454B}Prefabs/Characters/Factions/INDFOR/FIA/Character_FIA_Crew.et", "{641AD7731E23454B}Prefabs/Characters/Factions/INDFOR/FIA/Character_FIA_Crew.et" };
+		apcTypes.Set("US", usAPCs);
+		apcTypes.Set("USSR", ussrAPCs);
+		apcTypes.Set("FIA", fiaAPCs);
+		
 		populator.UpdateValues();
 		auto menu = GetGame().GetMenuManager().OpenMenu(ChimeraMenuPreset.PIE_LayoutPopulateArea);
 		SCR_PIE_PopulateAreaUI.Cast(menu).SetAreaPopulator(populator);
@@ -26,7 +61,7 @@ class PIE_AreaPopulator : Managed
 		foundBuildings.Clear();
 		FindBuildingsNearPoint(selectedMapItem.Entity().GetOrigin(), paramSpawnRadius);
 		valueFoundBuildings = foundBuildings.Count();
-		paramBuildingsToFill = Math.Min(paramBuildingsToFill, valueFoundBuildings - 1);
+		paramBuildingsToFill = Math.Max(Math.Min(paramBuildingsToFill, valueFoundBuildings - 1), 0);
 	}
 	
 	void Run()
@@ -34,25 +69,35 @@ class PIE_AreaPopulator : Managed
 		MapItem selectedMapItem = selectedMapDesc.Item();
 			
 		UpdateValues();
-
-		foreach (IEntity ent : foundBuildings)
+		
+		for(int i = 0; i < paramBuildingsToFill; i++)
 		{
-			SpawnUnit(selectedMapItem.Entity().GetOrigin(), ent);
+			SpawnGarrisonUnit(selectedMapItem.Entity().GetOrigin(), foundBuildings.GetRandomElement());
+		}
+		
+		for(int i = 0; i < paramPatrolsCount; i++)
+		{
+			SpawnPatrolUnit(selectedMapItem.Entity().GetOrigin());
+		}
+		
+		for(int i = 0; i < paramVehicleCount; i++)
+		{
+			SpawnVehicle(selectedMapItem.Entity().GetOrigin(), false);
 		}
 	}
 	
-	void SpawnUnit(vector spawnPos, IEntity ent)
+	void SpawnGarrisonUnit(vector townPos, IEntity ent)
 	{
 		vector position = ent.GetOrigin();
-		Resource groupPrefab = Resource.Load("{5BEA04939D148B1D}Prefabs/Groups/INDFOR/Group_FIA_FireTeam.et");
-		// Resource groupPrefab = Resource.Load("{84B40583F4D1B7A3}Prefabs/Characters/Factions/INDFOR/FIA/Character_FIA_Rifleman.et");
-		IEntity spawnedGroupEntity = GetGame().SpawnEntityPrefab(groupPrefab, GetGame().GetWorld(), GenerateSpawnParameters(spawnPos));
+		vector spawnPosition;
+		SCR_WorldTools.FindEmptyTerrainPosition(spawnPosition, position, 150);
+		Resource groupPrefab = Resource.Load(unitTypes.Get(paramSpawnUnitFaction).GetRandomElement());
+		IEntity spawnedGroupEntity = GetGame().SpawnEntityPrefab(groupPrefab, GetGame().GetWorld(), GenerateSpawnParameters(spawnPosition));
 
 		SCR_AIGroup group = SCR_AIGroup.Cast(spawnedGroupEntity);
 
 		if(group != null)
 		{
-			//Resource waypointResource = Resource.Load("{750A8D1695BD6998}Prefabs/AI/Waypoints/AIWaypoint_Move.et");
 			Resource waypointResource = Resource.Load("{93291E72AC23930F}Prefabs/AI/Waypoints/AIWaypoint_Defend.et");
 			vector waypointPos = position;
 			AIWaypoint waypoint = AIWaypoint.Cast(GetGame().SpawnEntityPrefab(waypointResource, null, GenerateSpawnParameters(waypointPos)));
@@ -67,6 +112,126 @@ class PIE_AreaPopulator : Managed
 		}
 	}
 
+	void SpawnPatrolUnit(vector townPos)
+	{
+		vector cycleWaypointPos = GetRandomOffsetPosition(townPos, 150);
+		Resource groupPrefab = Resource.Load(unitTypes.Get(paramSpawnUnitFaction).GetRandomElement());
+		IEntity spawnedGroupEntity = GetGame().SpawnEntityPrefab(groupPrefab, GetGame().GetWorld(), GenerateSpawnParameters(cycleWaypointPos));
+		
+		Resource moveWaypointResource = Resource.Load("{750A8D1695BD6998}Prefabs/AI/Waypoints/AIWaypoint_Move.et");
+		Resource cycleWaypointResource = Resource.Load("{35BD6541CBB8AC08}Prefabs/AI/Waypoints/AIWaypoint_Cycle.et");
+		SCR_AIGroup group = SCR_AIGroup.Cast(spawnedGroupEntity);
+		
+		AIWaypoint moveWaypoint = AIWaypoint.Cast(GetGame().SpawnEntityPrefab(moveWaypointResource, null, GenerateSpawnParameters(GetRandomOffsetPosition(townPos, 150))));
+		moveWaypoint.SetCompletionRadius(5.0);
+		
+		AIWaypoint moveCycleWaypoint = AIWaypoint.Cast(GetGame().SpawnEntityPrefab(moveWaypointResource, null, GenerateSpawnParameters(cycleWaypointPos)));
+		moveCycleWaypoint.SetCompletionRadius(5.0);
+		
+		AIWaypointCycle cycleWaypoint = AIWaypointCycle.Cast(GetGame().SpawnEntityPrefab(cycleWaypointResource, null, GenerateSpawnParameters(cycleWaypointPos)));
+		cycleWaypoint.SetWaypoints({moveWaypoint, moveCycleWaypoint});
+		cycleWaypoint.SetRerunCounter(10000);
+		group.AddWaypoint(cycleWaypoint);
+	}
+	
+	void SpawnVehicle(vector townPos, bool patrol)
+	{
+		array<string> spawnTypes = new array<string>();
+		if(paramVehicleJeep) 
+			spawnTypes.Insert("jeep");
+		
+		if(paramVehicleAPC)
+			spawnTypes.Insert("apc");
+		
+		if(spawnTypes.Count() == 0)
+			return;
+		
+		string vicType = spawnTypes.GetRandomElement();
+		string vicResourcePath;
+		array<string> crewResourcePaths = new array<string>();
+		
+		if(vicType == "jeep")
+		{
+			vicResourcePath = jeepTypes.Get(paramSpawnUnitFaction)[0];
+			for(int i = 1; i < jeepTypes.Get(paramSpawnUnitFaction).Count(); i++)
+			{
+				crewResourcePaths.Insert(jeepTypes.Get(paramSpawnUnitFaction)[i]);
+			}
+		}
+		else if (vicType == "apc")
+		{
+			vicResourcePath = apcTypes.Get(paramSpawnUnitFaction)[0];
+			for(int i = 1; i < apcTypes.Get(paramSpawnUnitFaction).Count(); i++)
+			{
+				crewResourcePaths.Insert(apcTypes.Get(paramSpawnUnitFaction)[i]);
+			}
+		}
+		
+		vector spawnPosition, spawnRotation;
+		// TODO: On road
+		GetRandomRoadSegmentTransform(townPos, 150, spawnPosition, spawnRotation);
+		SCR_WorldTools.FindEmptyTerrainPosition(spawnPosition, spawnPosition, 150);
+		
+		IEntity groupEntity = GetGame().SpawnEntityPrefab(Resource.Load("{000CD338713F2B5A}Prefabs/AI/Groups/Group_Base.et"), GetGame().GetWorld(), GenerateSpawnParameters(spawnPosition));
+		SCR_AIGroup aiGroup = SCR_AIGroup.Cast(groupEntity);
+		aiGroup.m_faction = paramSpawnUnitFaction;
+		
+		IEntity spawnedVehicleEntity = GetGame().SpawnEntityPrefab(Resource.Load(vicResourcePath), GetGame().GetWorld(), GenerateSpawnParameters(spawnPosition));
+		aiGroup.AddVehiclesStatic({ spawnedVehicleEntity.GetName() });
+		
+		foreach(string crew : crewResourcePaths)
+		{
+			IEntity spawnedCrewEntity = GetGame().SpawnEntityPrefab(Resource.Load(crew), GetGame().GetWorld(), GenerateSpawnParameters(spawnPosition));	
+			aiGroup.AddAIEntityToGroup(spawnedCrewEntity);
+		}
+		
+		AIWaypoint getInWaypoint = AIWaypoint.Cast(GetGame().SpawnEntityPrefab(Resource.Load("{B049D4C74FBC0C4D}Prefabs/AI/Waypoints/AIWaypoint_GetInNearest.et"), null, GenerateSpawnParameters(spawnPosition)));
+		// SCR_AIEntityWaypointParameters getInParams = SCR_AIEntityWaypointParameters.Cast(getInWaypoint.FindComponent(SCR_AIEntityWaypointParameters));
+		// getInParams.SetEntity(spawnedVehicleEntity);
+		aiGroup.AddWaypoint(getInWaypoint);
+	}
+	
+	static void GetRandomRoadSegmentTransform(
+		vector centerPos,
+		float offset,
+		out vector outPos,
+		out vector outDir
+	)
+	{
+		SCR_AIWorld aiWorld = SCR_AIWorld.Cast(GetGame().GetAIWorld());
+		if (!aiWorld)
+		{
+			return;
+		}
+
+		RoadNetworkManager roadNetworkManager = aiWorld.GetRoadNetworkManager();
+		array<BaseRoad> roads = new array<BaseRoad>();
+		roadNetworkManager.GetRoadsInAABB(
+			Vector(centerPos[0] - offset / 2, centerPos[1], centerPos[2] - offset / 2),
+			Vector(centerPos[0] + offset / 2, centerPos[1], centerPos[2] + offset / 2),
+			roads
+		);
+		
+		array<vector> roadPoints = new array<vector>();
+		roads.GetRandomElement().GetPoints(roadPoints);
+		
+		outPos = roadPoints.GetRandomElement();
+		outDir = vector.Zero;
+	}
+
+	static vector GetRandomOffsetPosition(vector center, float radius)
+	{
+		float angle = Math.RandomFloat(0.0, Math.PI2);
+		float dist  = Math.Sqrt(Math.RandomFloat(0.0, 1.0)) * radius;
+	
+		float x = center[0] + Math.Cos(angle) * dist;
+		float z = center[2] + Math.Sin(angle) * dist;
+
+		vector outPos;
+		SCR_WorldTools.FindEmptyTerrainPosition(outPos, Vector(x, center[1], z), 150);
+		return outPos;
+	}
+	
 	void FindBuildingsNearPoint(vector center, float radius)
 	{
 		BaseWorld world = GetGame().GetWorld();
--- scripts/Game/UI/Menu/SCR_PIE_PopulateAreaUI.c
+++ scripts/Game/UI/Menu/SCR_PIE_PopulateAreaUI.c
@@ -4,10 +4,10 @@ class SCR_PIE_PopulateAreaUI : MenuBase
 	protected static const string BUTTON_CHANGE = "ButtonStart";
 
 	ref PIE_AreaPopulator areaPopulator;
-	
+
 	private ref Widget rootWidget;
 	private bool inUIUpdate = false;
-	
+
 	//------------------------------------------------------------------------------------------------
 	protected override void OnMenuOpen()
 	{
@@ -19,7 +19,6 @@ class SCR_PIE_PopulateAreaUI : MenuBase
 		}
 
 		// Close button
-
 		SCR_ButtonTextComponent buttonClose = SCR_ButtonTextComponent.GetButtonText(BUTTON_CLOSE, rootWidget);
 		if (buttonClose)
 			buttonClose.m_OnClicked.Insert(Close);
@@ -27,29 +26,22 @@ class SCR_PIE_PopulateAreaUI : MenuBase
 			Print("Button Close not found - won't be able to exit by button", LogLevel.WARNING);
 
 		// Change button
-
 		SCR_ButtonTextComponent buttonChange = SCR_ButtonTextComponent.GetButtonText(BUTTON_CHANGE, rootWidget);
 		if (buttonChange)
 			buttonChange.m_OnClicked.Insert(RunSpawners);
 		else
-			Print("Button Change not found", LogLevel.WARNING); // the button can be missing without putting the layout in jeopardy
+			Print("Button Change not found", LogLevel.WARNING);
 
-		/*
-			ESC/Start listener
-		*/
-
+		// ESC listeners
 		InputManager inputManager = GetGame().GetInputManager();
 		if (inputManager)
 		{
-			// this is for the menu/dialog to close when pressing ESC
-			// an alternative is to have a button with the SCR_NavigationButtonComponent component
-			// and its Action Name field set to MenuBack - this would activate the button on ESC press
 			inputManager.AddActionListener("MenuOpen", EActionTrigger.DOWN, Close);
 			inputManager.AddActionListener("MenuBack", EActionTrigger.DOWN, Close);
-#ifdef WORKBENCH // in Workbench, F10 is used because ESC closes the preview
+#ifdef WORKBENCH
 			inputManager.AddActionListener("MenuOpenWB", EActionTrigger.DOWN, Close);
 			inputManager.AddActionListener("MenuBackWB", EActionTrigger.DOWN, Close);
-#endif // WORKBENCH
+#endif
 		}
 		else if (!buttonClose)
 		{
@@ -57,18 +49,21 @@ class SCR_PIE_PopulateAreaUI : MenuBase
 			Close();
 			return;
 		}
+
+		SCR_ButtonTextComponent.GetButtonText("ButtonUS", rootWidget, true).m_OnClicked.Insert(SetFactionUS);
+		SCR_ButtonTextComponent.GetButtonText("ButtonUSSR", rootWidget, true).m_OnClicked.Insert(SetFactionUSSR);
+		SCR_ButtonTextComponent.GetButtonText("ButtonFIA", rootWidget, true).m_OnClicked.Insert(SetFactionFIA);
 		
 		// Widget listeners
 		AddEventListeners("AreaSizeEdit");
 		AddEventListeners("AreaSizeSlider");
 		AddEventListeners("GarrisonEdit");
 		AddEventListeners("GarrisonSlider");
+		AddEventListeners("PatrolEdit");
 	}
 
-	//------------------------------------------------------------------------------------------------
 	protected override void OnMenuClose()
 	{
-		// here we clean action listeners added above as the good practice wants it
 		InputManager inputManager = GetGame().GetInputManager();
 		if (inputManager)
 		{
@@ -77,134 +72,220 @@ class SCR_PIE_PopulateAreaUI : MenuBase
 #ifdef WORKBENCH
 			inputManager.RemoveActionListener("MenuOpenWB", EActionTrigger.DOWN, Close);
 			inputManager.RemoveActionListener("MenuBackWB", EActionTrigger.DOWN, Close);
-#endif // WORKBENCH
+#endif
 		}
 	}
 
+	protected override void OnMenuUpdate(float tDelta)
+	{
+		if(areaPopulator)
+		{
+			ShapeFlags shapeFlags = ShapeFlags.ONCE | ShapeFlags.NOZBUFFER | ShapeFlags.TRANSP | ShapeFlags.DOUBLESIDE | ShapeFlags.NOOUTLINE;
+			vector trans[4];
+			areaPopulator.selectedMapDesc.Item().Entity().GetTransform(trans);
+			SCR_Shape.DrawCircle(trans, areaPopulator.paramSpawnRadius, ARGB(50, 200, 200, 200), ARGB(50, 200, 200, 200), shapeFlags);
+		}
+	}
+	
 	//------------------------------------------------------------------------------------------------
 	protected void RunSpawners()
 	{
 		areaPopulator.Run();
 		Close();
 	}
-	
+
 	void SetAreaPopulator(PIE_AreaPopulator populator)
 	{
 		areaPopulator = populator;
 		UpdateUI();
 	}
+
+	//------------------------------------------------------------------------------------------------
+	private void SetFactionUS()
+	{
+		areaPopulator.paramSpawnUnitFaction = "US";
+		UpdateUI();
+	}
+	
+	private void SetFactionUSSR()
+	{
+		areaPopulator.paramSpawnUnitFaction = "USSR";
+		UpdateUI();
+	}
+	
+	private void SetFactionFIA()
+	{
+		areaPopulator.paramSpawnUnitFaction = "FIA";
+		UpdateUI();
+	}
 	
 	private void AddEventListeners(string path)
 	{
-		SCR_EventHandlerComponent eventHandler = new SCR_EventHandlerComponent();
-		eventHandler.GetOnChange().Insert(WidgetOnChange);
-		eventHandler.GetOnChangeFinal().Insert(WidgetOnChangeFinal);
-		rootWidget.FindAnyWidget(path).AddHandler(eventHandler);
+		Widget w = rootWidget.FindAnyWidget(path);
+		if (!w)
+			return;
+
+		SCR_EventHandlerComponent handler = new SCR_EventHandlerComponent();
+		handler.GetOnChange().Insert(WidgetOnChange);
+		handler.GetOnChangeFinal().Insert(WidgetOnChangeFinal);
+
+		w.AddHandler(handler);
 	}
-	
+
+	//------------------------------------------------------------------------------------------------
 	void UpdateUI(string ignoreElement = "")
 	{
-		UpdateTextElement("TitleText", string.Format("Populate Area - %1", areaPopulator.selectedMapDesc.Item().GetDisplayName()));
-		
-		if(ignoreElement != "AreaSizeEdit")
-			UpdateEditTextElement("AreaSizeEdit", string.Format("%1", areaPopulator.paramSpawnRadius));
-		if(ignoreElement != "AreaSizeSlider")
+		inUIUpdate = true;
+
+		UpdateTextElement("TitleText",
+			string.Format("Populate Area - %1 - %2",
+			areaPopulator.paramSpawnUnitFaction,
+			areaPopulator.selectedMapDesc.Item().GetDisplayName()));
+
+		if (ignoreElement != "AreaSizeEdit")
+			UpdateEditTextElement("AreaSizeEdit", areaPopulator.paramSpawnRadius.ToString());
+
+		if (ignoreElement != "AreaSizeSlider")
 			UpdateSliderElement("AreaSizeSlider", areaPopulator.paramSpawnRadius, 0, 1000);
-		
-		if(ignoreElement != "GarrisonEdit")
-			UpdateEditTextElement("GarrisonEdit", string.Format("%1", areaPopulator.paramBuildingsToFill));
-		if(ignoreElement != "GarrisonSlider")
+
+		if (ignoreElement != "GarrisonEdit")
+			UpdateEditTextElement("GarrisonEdit", areaPopulator.paramBuildingsToFill.ToString());
+
+		if (ignoreElement != "GarrisonSlider")
 			UpdateSliderElement("GarrisonSlider", areaPopulator.paramBuildingsToFill, 0, areaPopulator.valueFoundBuildings);
-		UpdateTextElement("GarrisonMaxNumber", string.Format("%1", areaPopulator.valueFoundBuildings));
+
+		if (ignoreElement != "PatrolEdit")
+			UpdateEditTextElement("PatrolEdit", areaPopulator.paramPatrolsCount.ToString());
+		
+		UpdateTextElement("GarrisonMaxNumber", areaPopulator.valueFoundBuildings.ToString());
+
+		inUIUpdate = false;
 	}
-	
+
+	//------------------------------------------------------------------------------------------------
 	private void UpdateTextElement(string path, string value)
 	{
-		if (!rootWidget)
-			return;
-		
-		TextWidget textWidget = TextWidget.Cast(rootWidget.FindAnyWidget(path));
-		if (!textWidget)
-		{
-			PrintFormat("%1 as TextWidget could not be found", path, LogLevel.WARNING);
-			return;
-		}
-
-		textWidget.SetText(value);
+		TextWidget w = TextWidget.Cast(rootWidget.FindAnyWidget(path));
+		if (w)
+			w.SetText(value);
 	}
-	
+
 	private void UpdateEditTextElement(string path, string value)
 	{
-		if (!rootWidget)
-			return;
-		
-		EditBoxWidget textWidget = EditBoxWidget.Cast(rootWidget.FindAnyWidget(path));
-		if (!textWidget)
-		{
-			PrintFormat("%1 as EditBoxWidget could not be found", path, LogLevel.WARNING);
-			return;
-		}
-
-		textWidget.SetText(value);
+		EditBoxWidget w = EditBoxWidget.Cast(rootWidget.FindAnyWidget(path));
+		if (w)
+			w.SetText(value);
 	}
-	
+
 	private void UpdateSliderElement(string path, int value, int min = -1, int max = -1)
 	{
-		if (!rootWidget)
+		SliderWidget w = SliderWidget.Cast(rootWidget.FindAnyWidget(path));
+		if (!w)
 			return;
-		
-		SliderWidget widget = SliderWidget.Cast(rootWidget.FindAnyWidget(path));
-		if (!widget)
+
+		if (min >= 0 && max >= 0 && min <= max)
 		{
-			PrintFormat("%1 as SliderWidget could not be found", path, LogLevel.WARNING);
-			return;
-		}
-		
-		if(min >=0 && max >= 0 && min <= max)
-		{
-			widget.SetMin(min);
-			widget.SetMax(max);
+			w.SetMin(min);
+			w.SetMax(max);
 		}
 
-		widget.SetCurrent(value);
+		w.SetCurrent(value);
 	}
-	
+
+	//------------------------------------------------------------------------------------------------
 	protected void WidgetOnChange(Widget w)
 	{
-		if(inUIUpdate)
-		{
+		if (inUIUpdate)
 			return;
-		}
-		
-		inUIUpdate = true;
-		
-		switch(w.GetName())
+
+		string name = w.GetName();
+
+		switch (name)
 		{
 			case "AreaSizeSlider":
 				areaPopulator.paramSpawnRadius = SliderWidget.Cast(w).GetCurrent();
 				break;
+
 			case "AreaSizeEdit":
-				areaPopulator.paramSpawnRadius = EditBoxWidget.Cast(w).GetText().ToInt();
+			{
+				string txt = EditBoxWidget.Cast(w).GetText();
+				areaPopulator.paramSpawnRadius = txt.ToInt();
 				break;
+			}
+
 			case "GarrisonSlider":
 				areaPopulator.paramBuildingsToFill = SliderWidget.Cast(w).GetCurrent();
 				break;
+
 			case "GarrisonEdit":
-				areaPopulator.paramBuildingsToFill = EditBoxWidget.Cast(w).GetText().ToInt();
+			{
+				string txt = EditBoxWidget.Cast(w).GetText();
+				areaPopulator.paramBuildingsToFill = txt.ToInt();
 				break;
+			}
+			
+			case "PatrolEdit":
+			{
+				string txt = EditBoxWidget.Cast(w).GetText();
+				areaPopulator.paramPatrolsCount = txt.ToInt();
+				break;
+			}
 		}
-		
-		UpdateUI(w.GetName());
-		
+
+		inUIUpdate = true;
+		UpdateUI(name);
 		inUIUpdate = false;
 	}
-	
+
+	//------------------------------------------------------------------------------------------------
 	protected void WidgetOnChangeFinal(Widget w)
 	{
+		if (inUIUpdate)
+			return;
+
+		string name = w.GetName();
+
 		inUIUpdate = true;
-		WidgetOnChange(w);
-		inUIUpdate = false;
+
+		// Re-read widget value (do NOT call WidgetOnChange)
+		switch (name)
+		{
+			case "AreaSizeSlider":
+				areaPopulator.paramSpawnRadius = SliderWidget.Cast(w).GetCurrent();
+				break;
+
+			case "AreaSizeEdit":
+			{
+				string txt = EditBoxWidget.Cast(w).GetText();
+				areaPopulator.paramSpawnRadius = txt.ToInt();
+				break;
+			}
+
+			case "GarrisonSlider":
+				areaPopulator.paramBuildingsToFill = SliderWidget.Cast(w).GetCurrent();
+				break;
+
+			case "GarrisonEdit":
+			{
+				string txt = EditBoxWidget.Cast(w).GetText();
+				areaPopulator.paramBuildingsToFill = txt.ToInt();
+				break;
+			}
+			
+			case "PatrolEdit":
+			{
+				string txt = EditBoxWidget.Cast(w).GetText();
+				areaPopulator.paramPatrolsCount = txt.ToInt();
+				break;
+			}
+		}
+
+		// Recalculate after final change
 		areaPopulator.UpdateValues();
-		UpdateUI(w.GetName());
+
+		// Update UI with the new values
+		UpdateUI(name);
+
+		inUIUpdate = false;
 	}
 }
